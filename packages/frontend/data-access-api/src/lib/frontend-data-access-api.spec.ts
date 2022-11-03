2830168a49a0b3ba510430d74b0ec1715cd24cfb
JK2Kgit <jk2k@jk2k.xyz>
2022-11-03 18:42:57 +0100

    bindings

--- packages/frontend/data-access-api/src/lib/frontend-data-access-api.spec.ts
+++
@@ -1,7 +0,0 @@
-import { frontendDataAccessApi } from './frontend-data-access-api';
-
-describe('frontendDataAccessApi', () => {
-  it('should work', () => {
-    expect(frontendDataAccessApi()).toEqual('frontend-data-access-api');
-  });
-});
