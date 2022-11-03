0930f623fb58f5fbc54fa0caa2fb368035498139
JK2Kgit <jk2k@jk2k.xyz>
2022-11-03 17:14:48 +0100

    Database

--- packages/api-app/src/main.rs
+++ packages/api-app/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use prisma_client_rust::NewClientError;
+use crate::prisma::{GrammaticalForm, new_client, PrismaClient};
+
+mod prisma;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    let client: PrismaClient = new_client().await.unwrap(); // Update on new release
+    let user = client.user().create(
+        vec![],
+        false,
+        GrammaticalForm::Indeterminate,
+        vec![],
+    ).exec().await.unwrap();
 }
--- packages/prisma-cli/src/main.rs
+++ packages/prisma-cli/src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world!");
+    prisma_client_rust_cli::run();
 }
