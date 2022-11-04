5504e2b70bc887f457af34ec25fb14107a1d3525
PatrykGor <patryk@gorscy.net>
2022-11-04 16:27:28 +0100

    Removed unsupported config option

--- apps/erudio/vite.config.ts
+++ apps/erudio/vite.config.ts
@@ -2,8 +2,5 @@ import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
-  build: {
-    polyfillDynamicImport: false,
-  },
+  plugins: [solid({ ssr: false })]
 });
