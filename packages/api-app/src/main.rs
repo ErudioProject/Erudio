293bb09de766014ae2d48ed7021456023eccdfbf
JK2K <jk2k@jk2k.xyz>
2022-11-04 14:49:37 +0100

    Feature/register (#1)
    * register
    merge when i get rid of .unwrap()
    
    * improvments

--- packages/api-app/src/main.rs
+++ packages/api-app/src/main.rs
@@ -2,24 +2,20 @@ extern crate argon2;
 
 mod prisma;
 mod routes;
+mod shutdown_signal;
 
-use prisma_client_rust::NewClientError;
-use crate::prisma::{GrammaticalForm, new_client, PrismaClient};
-use std::env;
-use std::net::{Ipv4Addr, SocketAddr};
-use std::path::PathBuf;
-use std::sync::Arc;
-use axum::extract::Path;
+use crate::eyre::eyre;
+use crate::prisma::{new_client, GrammaticalForm, PrismaClient};
+use crate::routes::{router, Ctx};
 use axum::routing::get;
 use color_eyre::eyre;
-use log::{debug, error, info};
-use rspc::Config;
-use tokio::signal;
-use tower_cookies::{Cookie, CookieManagerLayer, Cookies};
+use log::{error, info, trace};
+use std::env;
+use std::net::{Ipv4Addr, SocketAddr};
+use std::sync::Arc;
+use tower_cookies::{CookieManagerLayer, Cookies};
 use tower_http::cors;
 use tower_http::cors::CorsLayer;
-use crate::routes::{Ctx, router};
-
 
 // TODO clean up a bit
 pub fn main() {
@@ -27,65 +23,45 @@ pub fn main() {
     env_logger::init();
     let result = start();
     if let Some(err) = result.err() {
-        error!("{}", err);
+        error!("{:?}", err);
     }
 }
 
-
 #[tokio::main]
 async fn start() -> eyre::Result<()> {
-    let client: Arc<PrismaClient> = Arc::new(new_client().await?); // Update on new release
+    let client: Arc<PrismaClient> = Arc::new(
+        new_client()
+            .await
+            .map_err(|err| eyre!("Database client error: {:?}", err))?,
+    ); // Update on new release
     let router = router().arced();
 
     let app = axum::Router::new()
-        .route("/", get(|| async { "Hello 'rspc'!" }))
-        .route("/rspc/:id", router.endpoint(move |cookies: Cookies| {
-            Ctx {
-                db: client.clone(),
-                cookies
-            }
-        }).axum())
+        .route("/", get(|| async { "Erudio" }))
+        .route(
+            "/rspc/:id",
+            router
+                .endpoint(move |cookies: Cookies| Ctx {
+                    db: client.clone(),
+                    cookies,
+                })
+                .axum(),
+        )
         .layer(
             CorsLayer::new()
                 .allow_origin(cors::Any)
                 .allow_headers(cors::Any)
                 .allow_methods(cors::Any),
         )
-        .layer(CookieManagerLayer::new());;
+        .layer(CookieManagerLayer::new());
 
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3000));
 
     info!("listening on {}", addr);
     axum::Server::try_bind(&addr)?
         .serve(app.into_make_service())
-        .with_graceful_shutdown(shutdown_signal())
+        .with_graceful_shutdown(shutdown_signal::shutdown_signal())
         .await?;
 
     Ok(())
-}
-
-async fn shutdown_signal() {
-    let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
-    };
-
-    #[cfg(unix)]
-        let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
-    };
-
-    #[cfg(not(unix))]
-        let terminate = std::future::pending::<()>();
-
-    tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
-    }
-
-    info!("signal received, starting graceful shutdown");
 }
