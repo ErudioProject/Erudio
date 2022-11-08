c19c52ee8a49af74b664cb09c4d7728f57776c4f
PatrykGor <patryk@gorscy.net>
2022-11-08 09:04:53 +0100

    Added Hope-UI

--- apps/erudio/src/root.tsx
+++ apps/erudio/src/root.tsx
@@ -1,39 +1,44 @@
 // @refresh reload
-import { Suspense } from "solid-js";
+import { Show, Suspense } from "solid-js";
+import { ColorModeScript, HopeProvider, injectCriticalStyle } from '@hope-ui/core'
 import {
-  A,
-  Body,
-  ErrorBoundary,
-  FileRoutes,
-  Head,
-  Html,
-  Meta,
-  Routes,
-  Scripts,
-  Title,
+    A,
+    Body,
+    ErrorBoundary,
+    FileRoutes,
+    Head,
+    Html,
+    Meta,
+    Routes,
+    Scripts,
+    Title,
 } from "solid-start";
 import "./root.css";
 
 export default function Root() {
-  return (
-    <Html lang="en">
-      <Head>
-        <Title>SolidStart - Bare</Title>
-        <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Body>
-        <Suspense>
-          <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-          </ErrorBoundary>
-        </Suspense>
-        <Scripts />
-      </Body>
-    </Html>
-  );
+    injectCriticalStyle();
+    return (
+        <Html lang="en">
+            <Head>
+                <Title>SolidStart - Bare</Title>
+                <Meta charset="utf-8" />
+                <Meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <Body>
+                <ColorModeScript />
+                <HopeProvider>
+                    <Suspense>
+                        <ErrorBoundary>
+                            <A href="/">Index</A>
+                            <A href="/about">About</A>
+                            <Routes>
+                                <FileRoutes />
+                            </Routes>
+                        </ErrorBoundary>
+                    </Suspense>
+                </HopeProvider>
+                <Scripts />
+            </Body>
+        </Html>
+    );
 }
