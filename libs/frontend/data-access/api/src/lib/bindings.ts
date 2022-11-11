15c97c8d56601f37450356259e451b3435233370
JK2K <jk2k@jk2k.xyz>
2022-11-11 09:30:05 +0100

    update to accomodate upstream fix

--- libs/frontend/data-access/api/src/lib/bindings.ts
+++ libs/frontend/data-access/api/src/lib/bindings.ts
@@ -4,7 +4,7 @@ export type Procedures = {
     queries: 
         { key: "public.login", input: LoginRequest, result: LoginResponse } | 
         { key: "public.version", input: never, result: string } | 
-        { key: "user.me", input: never, result: { two_factor_auth: boolean, grammatical_form: GrammaticalForm, pii_data: { email: string | null, pesel: string | null, birth_date: string | null, legal_name: string | null, display_name: string | null, phone_prefix: string | null, phone_number: string | null } | null } },
+        { key: "user.me", input: never, result: { two_factor_auth: boolean, pii_data: { grammatical_form: GrammaticalForm, email: string | null, pesel: string | null, birth_date: string | null, legal_name: string | null, display_name: string | null, phone_prefix: string | null, phone_number: string | null } | null } },
     mutations: 
         { key: "public.register", input: RegisterRequest, result: null },
     subscriptions: never
