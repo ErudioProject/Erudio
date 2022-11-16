510ae89cb4c18e459d1b83fa7191656e040b3e3a
JK2K <jk2k@jk2k.xyz>
2022-11-16 21:05:13 +0100

    actual semantic versioning regex - I know it is total overkill

--- libs/frontend/data-access/api/src/lib/frontend-data-access-api.spec.ts
+++ libs/frontend/data-access/api/src/lib/frontend-data-access-api.spec.ts
@@ -7,6 +7,6 @@ const client = createFetchApiClient(process.env.NX_API_URL!)
 describe("createFetchApiClient", () => {
     it("returns version correctly", async () => {
         const res = await client.query(["public.version"]);
-        expect(res).toMatch(/\d+\.\d+\.\d+/);
+        expect(res).toMatch(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/);
     })
 })
