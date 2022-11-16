f5ffff88a3e5fbb900f311ffbc265f79f4460da7
JK2K <jk2k@jk2k.xyz>
2022-11-16 17:16:47 +0100

    docker fix

--- apps/api/src/routes/public/version.rs
+++ apps/api/src/routes/public/version.rs
@@ -1,5 +1,5 @@
 use crate::{routes::RspcResult, Ctx};
 
 pub(crate) async fn version(_: Ctx, _: ()) -> RspcResult<String> {
-	Ok(env!("CARGO_PKG_VERSION").to_string()) as RspcResult<String>
+	Ok(env!("CARGO_PKG_VERSION").to_string())
 }
