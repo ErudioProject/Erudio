2fb740972f417997e04f52ceea1434d0f55b1cb4
JK2K <jk2k@jk2k.xyz>
2022-11-16 20:57:02 +0100

    fix .env

--- libs/frontend/data-access/api/src/lib/frontend-data-access-api.spec.ts
+++ libs/frontend/data-access/api/src/lib/frontend-data-access-api.spec.ts
@@ -1,14 +1,8 @@
 import { createFetchApiClient } from "./frontend-data-access-api";
 import 'isomorphic-fetch';
 
-// TODO patryk you are fixing it move it to jest.init.ts or somewhere
-import { config } from 'dotenv';
-if (!process.env['NX_INVOKED_BY_RUNNER']) {
-    config({ path: __dirname + '/.env' });
-}
-
 global.fetch = fetch
-const client = createFetchApiClient(process.env.VITE_API_URL!)
+const client = createFetchApiClient(process.env.NX_API_URL!)
 
 describe("createFetchApiClient", () => {
     it("returns version correctly", async () => {
