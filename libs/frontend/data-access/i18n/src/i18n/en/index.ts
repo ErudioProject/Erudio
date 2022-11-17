8b47f08d6e4f8e8cb34d09d0f3a68e33866aaa80
PatrykGor <patryk@gorscy.net>
2022-11-17 19:11:49 +0100

    Ran prettier

--- libs/frontend/data-access/i18n/src/i18n/en/index.ts
+++ libs/frontend/data-access/i18n/src/i18n/en/index.ts
@@ -2,7 +2,7 @@ import type { Translation } from '../i18n-types';
 import pl from '../pl';
 
 const en: Translation = {
-  ...pl as Translation,
+  ...(pl as Translation),
   HI: 'Hi {name:string}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n',
 };
 
