7fd0f3a6e9ce6785760700af954c27a20e7ed14e
JK2K <jk2k@jk2k.xyz>
2022-11-18 15:41:56 +0100

    remove nx, rearrange

--- libs/frontend/features/authentication/src/components/sesssion-flow/sesssion-flow.spec.tsx
+++
@@ -1,8 +0,0 @@
-import SesssionFlow from './SesssionFlow';
-import { render } from 'solid-testing-library';
-
-it('it works', async () => {
-  const { getByText } = render(SesssionFlow);
-
-  expect(getByText('Hello component!'));
-});
