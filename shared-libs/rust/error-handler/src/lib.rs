cc2049ee6984f14d8f756663e11863473a0b59ab
JK2K <jk2k@jk2k.xyz>
2022-12-02 17:27:49 +0100

    cleanup

--- shared-libs/rust/error-handler/src/lib.rs
+++ shared-libs/rust/error-handler/src/lib.rs
@@ -1,17 +1,3 @@
 #![forbid(unsafe_code)]
 pub mod error_mapping;
 pub use error_mapping::*;
-
-pub fn error_handler() -> String {
-	"error_handler".into()
-}
-
-#[cfg(test)]
-mod tests {
-	use super::*;
-
-	#[test]
-	fn it_works() {
-		assert_eq!(error_handler(), "error_handler".to_string());
-	}
-}
