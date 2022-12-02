901bb8b9288e32539676bb60e39be3d0550fc0c7
PatrykGor <patryk@gorscy.net>
2022-12-02 18:49:27 +0100

    Added browser api mocking

--- apps/erudio/frontend/src/entry-client.tsx
+++ apps/erudio/frontend/src/entry-client.tsx
@@ -1,3 +1,8 @@
 import { mount, StartClient } from 'solid-start/entry-client';
+import { worker } from './test/mocks/browser';
+
+if (import.meta.env.DEV) {
+    worker.start()
+}
 
 mount(() => <StartClient />, document);
--- apps/erudio/frontend/src/test/mocks/handlers.ts
+++ apps/erudio/frontend/src/test/mocks/handlers.ts
@@ -1,31 +1,99 @@
-import { rest } from "msw";
-import { LoginRequest, LoginResponse } from "../../../../bindings";
+import { rest, RestRequest } from "msw";
+import { LoginRequest, LoginResponse, SchoolRelationType } from "../../../../bindings";
 
-const url = process.env.VITE_API_URL;
+const url = "http://127.0.0.1:3001/rspc" //process.env.FRONTEND_API_URL;
 
-const testData = {
-    authorizedMail: "Email",
-    authorizedPassword: "Password"
+export const apiTestData = {
+    authorizedMail: "test@example.com",
+    authorizedPassword: "Test123!",
+
+    displayName: "Test User",
+    schoolRelations: [
+        {
+            school_relation_type: "student" as SchoolRelationType,
+            school: { name: "My School" }
+        }, {
+            school_relation_type: "teacher" as SchoolRelationType,
+            school: { name: "My School 2" }
+        }
+    ]
 }
 
+interface RSPCError {
+    code: number,
+    message: string,
+    data: object | null
+}
+
+function wrapResponse<T>(data: T) {
+    return {
+        jsonrpc: "2.0",
+        id: null,
+        result: {
+            type: "response",
+            data
+        }
+    }
+}
+
+function wrapError(data: RSPCError) {
+    return {
+        jsonrpc: "2.0",
+        id: null,
+        result: {
+            type: "error",
+            data
+        }
+    }
+}
+
+function getData<T>(req: RestRequest): T {
+    return JSON.parse(req.url.searchParams.get("input") ?? "{}") as T
+}
+
+type UserMeResponse = { pii_data: { display_name: string } | null, user_school_relation: Array<{ school_relation_type: SchoolRelationType, school: { name: string } }> }
+
 export const handlers = [
-    rest.get(`${url}/public.login`, async (req, res, ctx) => {
-        const data = await req.json() as LoginRequest;
-        if (data.email === testData.authorizedMail && data.password === testData.authorizedPassword) {
+    rest.get(`${url}/user.me`, (_, res, ctx) => {
+        if (sessionStorage.getItem('is-authenticated') === 'true') {
+            return res(
+                ctx.status(200),
+                ctx.json(wrapResponse<UserMeResponse>({
+                    pii_data: {
+                        display_name: apiTestData.displayName
+                    },
+                    user_school_relation: apiTestData.schoolRelations
+                }))
+            )
+        }
+        else {
+            return res(
+                ctx.status(200),
+                ctx.json(wrapError({
+                    code: 401,
+                    message: "Unauthorized",
+                    data: null
+                }))
+            )
+        }
+    }),
+    rest.get(`${url}/public.login`, (req, res, ctx) => {
+        const data = getData<LoginRequest>(req);
+        if (data.email === apiTestData.authorizedMail && data.password === apiTestData.authorizedPassword) {
             sessionStorage.setItem('is-authenticated', 'true')
             return res(
                 ctx.status(200),
-                ctx.json({
-                    t: "Success"
-                } as LoginResponse)
+                ctx.json(wrapResponse<LoginResponse>({ t: "Success" }))
             );
         }
         else
             return res(
-                ctx.status(404)
+                ctx.status(200),
+                ctx.json(wrapError({
+                    code: 404,
+                    message: "Email not found",
+                    data: null
+                }))
             );
-    }),
-
-    rest.get(`${url}/user.me`, (_, res,) => {
     }),
 ]
