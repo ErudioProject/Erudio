fa7d47a5d572396f6976820e6e74153792b6b9e5
PatrykGor <patryk@gorscy.net>
2023-02-06 10:59:37 +0100

    Refactored and added translations to pre-dashboard

--- apps/erudio/frontend/src/routes/(auth)/dashboard.tsx
+++ apps/erudio/frontend/src/routes/(auth)/dashboard.tsx
@@ -1,36 +1,51 @@
-import { Component, For, Show, Suspense } from "solid-js";
+import { Component, For, Match, Show, Switch } from "solid-js";
 import { A, Navigate } from "solid-start";
 import { SchoolRelationType } from "../../../../bindings";
-import rspc from "../../api-setup";
+import { useI18nContext } from "../../i18n/i18n-solid";
+import createSession from "../../lib/session";
 
 const SchoolCard: Component<{ school: [SchoolRelationType, string] }> = (props) => {
+    const { LL } = useI18nContext()
     return (
         <div class="card border border-primary text-primary hover:text-primary-focus hover:border-primary-focus">
             <div class="card-body">
                 <h2 class="card-title truncate">{props.school[1]}</h2>
-                <p>{props.school[0]}</p>
+                <p>
+                    <Switch>
+                        <Match when={props.school[0] === "teacher"}>
+                            {LL().dashboard.teacher()}
+                        </Match>
+                        <Match when={props.school[0] === "student"}>
+                            {LL().dashboard.student()}
+                        </Match>
+                        <Match when={props.school[0] === "admin"}>
+                            {LL().dashboard.admin()}
+                        </Match>
+                        <Match when={props.school[0] === "director"}>
+                            {LL().dashboard.director()}
+                        </Match>
+                    </Switch>
+                </p>
             </div>
         </div>
     )
 }
 
 export default function Dashboard() {
-    const me = rspc.createQuery(() => ["user.me"]);
+    const session = createSession();
     return (
         <>
-            <Show when={me.data?.school_relations.length === 1}>
-                <Navigate href={`/user/${encodeURIComponent(me.data?.school_relations[0][1]!)}`} />
+            <Show when={session.data?.school_relations.length === 1}>
+                <Navigate href={`/${encodeURIComponent(session.data!.school_relations[0][1]!)}/${session.data!.school_relations[0][0]}`} />
             </Show>
             <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 m-3">
-                <Suspense>
-                    <For each={me.data?.school_relations}>
-                        {school =>
-                            <A href={`/user/${encodeURIComponent(school[1])}`}>
-                                <SchoolCard school={school} />
-                            </A>
-                        }
-                    </For>
-                </Suspense>
+                <For each={session.data?.school_relations}>
+                    {school =>
+                        <A href={`/${encodeURIComponent(school[1])}/${school[0]}`}>
+                            <SchoolCard school={school} />
+                        </A>
+                    }
+                </For>
             </div>
         </>
     )
