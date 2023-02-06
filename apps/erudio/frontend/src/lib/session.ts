3448b2fb8c52ac8272a721a4366ec496d49ca8a4
PatrykGor <patryk@gorscy.net>
2023-02-06 19:39:01 +0100

    Fixed createSession issue and added createAdminSession

--- apps/erudio/frontend/src/lib/session.ts
+++ apps/erudio/frontend/src/lib/session.ts
@@ -1,9 +1,24 @@
+import { RSPCError } from "@rspc/client";
+import { CreateQueryResult } from "@tanstack/solid-query";
+import { UserMeResponse } from "../../../bindings";
 import rspc from "../api-setup";
 
-const createSession = (login = false) => rspc.createQuery(() => ['user.me'], {
+const createSession = (login = false): CreateQueryResult<UserMeResponse, RSPCError> => rspc.createQuery(() => ['user.me'], {
     retry: login ? false : 3,
     refetchOnWindowFocus: !login,
-    refetchInterval: login ? false : 1000 * 60 * 5
+    refetchInterval: login ? false : 1000 * 60 * 5,
+    retryOnMount: false
 })
+export const createAdminSession = (login = false): [CreateQueryResult<string, RSPCError>, CreateQueryResult<UserMeResponse, RSPCError>] => {
+    const adminData = rspc.createQuery(() => ['super_admin.version'], {
+        retry: login ? false : 3,
+        refetchOnWindowFocus: !login,
+        refetchInterval: login ? false : 1000 * 60 * 5,
+        retryOnMount: false
+    });
+    const userData = createSession(login);
+
+    return [adminData, userData]
+}
 
 export default createSession;
