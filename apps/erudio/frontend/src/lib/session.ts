d979d02cc7c5e12e6dd3950ca4b603427b73f71e
PatrykGor <patryk@gorscy.net>
2023-02-06 10:59:26 +0100

    Added refetch interval to session

--- apps/erudio/frontend/src/lib/session.ts
+++ apps/erudio/frontend/src/lib/session.ts
@@ -1,5 +1,9 @@
 import rspc from "../api-setup";
 
-const createSession = (login = false) => rspc.createQuery(() => ['user.me'], { retry: login ? false : 3, refetchOnWindowFocus: !login })
+const createSession = (login = false) => rspc.createQuery(() => ['user.me'], {
+    retry: login ? false : 3,
+    refetchOnWindowFocus: !login,
+    refetchInterval: login ? false : 1000 * 60 * 5
+})
 
 export default createSession;
