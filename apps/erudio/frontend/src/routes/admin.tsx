8db9425fccc77a201640433fe8f760c80d3793e0
PatrykGor <patryk@gorscy.net>
2023-02-06 19:39:34 +0100

    Added admin login page

---
+++ apps/erudio/frontend/src/routes/admin.tsx
@@ -0,0 +1,18 @@
+import { Show } from "solid-js";
+import { Navigate, Outlet } from "solid-start";
+import Nav from "../components/Nav";
+import { createAdminSession } from "../lib/session";
+export default function AdminLayout() {
+    const [admin, session] = createAdminSession();
+    return (
+        <>
+            <Show when={admin.isError}>
+                <Navigate href="/admin/login" />
+            </Show>
+            <Show when={session.data}>
+                <Nav displayName={session.data!.display_name} userId={session.data!.id} />
+            </Show>
+            <Outlet />
+        </>
+    );
+}
