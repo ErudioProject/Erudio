76527df4c7fb4641bc8631ff626011264300a0fd
PatrykGor <patryk@gorscy.net>
2023-02-08 19:37:04 +0100

    Added adminEndpoints mocks

--- apps/erudio/frontend/src/test/mocks/handlers.ts
+++ apps/erudio/frontend/src/test/mocks/handlers.ts
@@ -1,5 +1,5 @@
 import { rest, RestRequest } from "msw";
-import { LoginRequest, LoginResponse, SchoolRelationType, UserMeResponse } from "../../../../bindings";
+import { AdminLoginRequest, AdminLoginResponse, LoginRequest, LoginResponse, School, UserMeResponse } from "../../../../bindings";
 import clientEnv from "../../lib/env";
 import { apiTestData } from "../data";
 
@@ -43,6 +43,36 @@ async function postData<T>(req: RestRequest): Promise<T> {
 
 export const handlers = [
     rest.get(`${url}/super_admin.searchSchools`, (_, res, ctx) => {
+        if (sessionStorage.getItem('is-admin') === 'true') {
+            return res(
+                ctx.status(200),
+                ctx.json(wrapResponse<Array<School>>(apiTestData.schools))
+            )
+        }
+        return res(
+            ctx.status(200),
+            ctx.json(wrapError({
+                code: 401,
+                message: "Unauthorized",
+                data: null
+            }))
+        )
+    }),
+    rest.get(`${url}/super_admin.version`, (_, res, ctx) => {
+        if (sessionStorage.getItem('is-admin') === 'true') {
+            return res(
+                ctx.status(200),
+                ctx.json(wrapResponse<string>("0.0.1"))
+            )
+        }
+        return res(
+            ctx.status(200),
+            ctx.json(wrapError({
+                code: 401,
+                message: "Unauthorized",
+                data: null
+            }))
+        )
     }),
     rest.get(`${url}/user.me`, (_, res, ctx) => {
         if (sessionStorage.getItem('is-authenticated') === 'true') {
@@ -50,21 +80,19 @@ export const handlers = [
                 ctx.status(200),
                 ctx.json(wrapResponse<UserMeResponse>({
                     display_name: apiTestData.displayName,
-                    school_relations: apiTestData.schoolRelations as [SchoolRelationType, string][],
+                    school_relations: apiTestData.schoolRelations,
                     id: apiTestData.userId
                 }))
             )
         }
-        else {
-            return res(
-                ctx.status(200),
-                ctx.json(wrapError({
-                    code: 401,
-                    message: "Unauthorized",
-                    data: null
-                }))
-            )
-        }
+        return res(
+            ctx.status(200),
+            ctx.json(wrapError({
+                code: 401,
+                message: "Unauthorized",
+                data: null
+            }))
+        )
     }),
     rest.post(`${url}/public.login`, async (req, res, ctx) => {
         const data = await postData<LoginRequest>(req);
@@ -73,6 +101,26 @@ export const handlers = [
             return res(
                 ctx.status(200),
                 ctx.json(wrapResponse<LoginResponse>({ t: "Success" }))
+            );
+        }
+        else
+            return res(
+                ctx.status(200),
+                ctx.json(wrapError({
+                    code: 404,
+                    message: "Email not found",
+                    data: null
+                }))
+            );
+    }),
+    rest.post(`${url}/public.login.admin`, async (req, res, ctx) => {
+        const data = await postData<AdminLoginRequest>(req);
+        if (data.login === apiTestData.authorizedMail && data.password === apiTestData.authorizedPassword) {
+            sessionStorage.setItem('is-admin', 'true')
+            sessionStorage.setItem('is-authenticated', 'true')
+            return res(
+                ctx.status(200),
+                ctx.json(wrapResponse<AdminLoginResponse>({ t: "Success" }))
             );
         }
         else
