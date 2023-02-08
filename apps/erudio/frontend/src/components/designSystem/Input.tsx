f51af7a16591cc47eca4e22caebfa21540dfd43d
PatrykGor <patryk@gorscy.net>
2023-02-08 12:51:44 +0100

    Made TextInput work without a form/more general

--- apps/erudio/frontend/src/components/designSystem/Input.tsx
+++ apps/erudio/frontend/src/components/designSystem/Input.tsx
@@ -1,12 +1,13 @@
-import { Show, VoidComponent } from "solid-js"
+import { JSX, Show, VoidComponent } from "solid-js"
 
 type TextInputProps = {
-    type: string,
-    required: boolean,
-    disabled: boolean,
-    name: string,
+    type?: string,
+    required?: boolean,
+    disabled?: boolean,
+    name?: string,
+    onInput?: JSX.EventHandlerUnion<HTMLInputElement, InputEvent>
     display: string,
-    errors: string[] | null
+    errors?: string[] | null
 }
 
 export const TextInput: VoidComponent<TextInputProps> = (props) => (
@@ -16,7 +17,7 @@ export const TextInput: VoidComponent<TextInputProps> = (props) => (
                 {props.display}
             </span>
         </label>
-        <input type={props.type} required={props.required} disabled={props.disabled} name={props.name} id={props.name} classList={{ "input-error text-error": props.errors !== null }} class="input input-primary input-bordered" />
+        <input type={props.type ?? "text"} required={props.required} disabled={props.disabled} name={props.name ?? props.display} id={props.name} classList={{ "input-error text-error": props.errors !== null }} class="input input-primary input-bordered" onInput={() => props.onInput} />
         <Show when={props.errors !== null}>
             <label class="label">
                 <span class="label-text-alt text-error">{props.errors?.[0]}</span>
